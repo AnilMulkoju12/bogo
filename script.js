4d583c7d19a35d093c0fe7dea3f7cde94ca7ae3d
AnilMulkoju12 <anilmulkoju123rose@gmail.com>
2024-11-14 22:25:03 +0530

    new change added

--- script.js
+++ script.js
@@ -10,35 +10,44 @@ var mostPopularContainer = document.querySelector(".most-popular-container");
 
 var thirdContainer = document.querySelector(".third-container-top-section");
 var thirdBottomContainer = document.querySelector(".third-container-bottom-section");
-var thirdInput = document.querySelector(".third-input"); // Assuming you have a third radio button or similar element
+var thirdInput = document.querySelector(".third-input");
 
 // Initially hide all containers
 firstBottomContainer.style.display = "none";
 secondBottomContainer.style.display = "none";
 thirdBottomContainer.style.display = "none";
 
-firstContainer.addEventListener("click", () => {
-  // Close other containers
-  secondBottomContainer.style.display = "none";
+// Function to clear all inputs
+function clearAllInputs() {
+  firstInput.checked = false;
   secondInput.checked = false;
+  thirdInput.checked = false;
+}
+
+// Toggle the first container
+firstContainer.addEventListener("click", () => {
+  // Close other containers and clear inputs
+  secondBottomContainer.style.display = "none";
   thirdBottomContainer.style.display = "none";
-  mostPopularContainer.style.top = "-20%";
+  clearAllInputs();
 
   // Toggle the first container
   if (firstBottomContainer.style.display === "none") {
     firstBottomContainer.style.display = "";
     firstInput.checked = true;
+    mostPopularContainer.style.top = "-20%";
   } else {
     firstBottomContainer.style.display = "none";
     firstInput.checked = false;
   }
 });
 
+// Toggle the second container
 secondContainer.addEventListener("click", () => {
-  // Close other containers
+  // Close other containers and clear inputs
   firstBottomContainer.style.display = "none";
-  firstInput.checked = false;
   thirdBottomContainer.style.display = "none";
+  clearAllInputs();
 
   // Toggle the second container
   if (secondBottomContainer.style.display === "none") {
@@ -52,18 +61,18 @@ secondContainer.addEventListener("click", () => {
   }
 });
 
+// Toggle the third container
 thirdContainer.addEventListener("click", () => {
-  // Close other containers
+  // Close other containers and clear inputs
   firstBottomContainer.style.display = "none";
-  firstInput.checked = false;
   secondBottomContainer.style.display = "none";
-  secondInput.checked = false;
-  mostPopularContainer.style.top = "-20%"; // Reset position if needed
+  clearAllInputs();
 
   // Toggle the third container
   if (thirdBottomContainer.style.display === "none") {
     thirdBottomContainer.style.display = "";
     thirdInput.checked = true;
+    mostPopularContainer.style.top = "-20%";
   } else {
     thirdBottomContainer.style.display = "none";
     thirdInput.checked = false;
